0da148e22cc4e89b912b0116642f7f9b73932a6a
Finlay McLees <finlay.mclees@gmail.com>
2018-04-15 22:00:24 +0100

    User Dropdown List Functionality

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,48 @@
+// Detects if the user has clicked on the dropdown menu for user edit
+var userListDropDown = document.getElementById('userList');
+
+var firstNameEditForm = document.getElementById('editFirstName');
+var lastNameEditForm = document.getElementById('editLastName');
+var ageEditForm = document.getElementById('editAge');
+var emailEditForm = document.getElementById('editEmail');
+var PBL2EditForm = document.getElementById('editPBL2');
+var SBEditForm = document.getElementById('editSB');
+var FAEditForm = document.getElementById('editFA');
+var MLEditForm = document.getElementById('editML');
+
+userFetch();
+
+// User functions
+// Fetches the user json list using an XML Http request
 function userFetch() {
   var usersRequest = new XMLHttpRequest();
 
-  usersRequest.open('GET', 'https://raw.githubusercontent.com/FinMcLees/RescueRota---Basic/master/resources/db/data.json');
+  usersRequest.open('GET', 'https://raw.githubusercontent.com/FinMcLees/RescueRota---Basic/master/resources/db/users.json');
   usersRequest.onload = function() {
-    var data = JSON.parse(usersRequest.responseText);
-    console.log(data[0]);
+    var recievedData = JSON.parse(usersRequest.responseText);
+    renderUsersList(recievedData);
   };
 
   usersRequest.send();
+  return data;
 }
 
+function renderUsersList(data){
+  var userString = "";
+
+  for (var i = 0; i < data.length; i++) {
+    userString = "<option id='selectNo"i"'>" + data[i].firstName + " " + data[i].lastName + "</option>";
+    userListDropDown.insertAdjacentHTML('beforeend', userString);
+    }
+}
+
+function renderUserData(data){
+  console.log('User Selected');
+  data
+}
+
+// Event functions
+// Fetches the event json list using an XML Http request
 function eventFetch(){
   var eventRequest = new XMLHttpRequest();
 
@@ -22,6 +55,8 @@ function eventFetch(){
   eventRequest.send();
 }
 
+//Series Functions
+//Fetch the series json list using an XML Http request
 function seriesFetch(){
   var seriesRequest = new XMLHttpRequest();
 
@@ -32,20 +67,4 @@ function seriesFetch(){
   };
 
   seriesRequest.send();
-}
-
-function renderUsersList(data){
-  for (var i = 0; i < data.length; i++) {
-    }
-
-function usersUpdate(){
-
-}
-
-function eventUpdate(){
-
-}
-
-function seriesUpdate(){
-
 }
